4e284c838959acee403b63d4318960e2268a9fd4
Gorgeous568 <iregbeyenm@gmail.com>
2025-05-15 16:30:19 +0100

    script

---
+++ New java/script.js
@@ -0,0 +1,70 @@
+ 
+//  function greet(name, gender, score){
+//             let name = name;
+//             console.log("Hello, world",name, gender);
+//         }
+//         greet('ife','male',90)
+//         greet('sam', 'male')
+
+//         prompt('Enter any number')
+//         function takeinputFromUser(){
+//             prompt('Enter any number');
+//         }
+//         takeinputFromUser()
+
+//         // function expression
+//         let sumof3Numbers= function(num1, num2, num3) {
+//             let result = num1 + num2 + num3;
+//             console.log(SpeechRecognitionResult)
+//         }
+
+//         // Class work
+
+//         prompt(subtract two numbers)
+//         let subof32Numbers=function(sub1 - sub2)
+
+        
+        // function takeinPutFromUser (){
+        //     prompt ('Enter any number > 6')
+        // }
+        // let userResponse = prompt(Enter any number > 6)
+        // console.log(5)
+        // let myName = 'ife'
+
+        // function checkNumber(){
+        //     let num = prompt ('Enter a number');
+        //     if(num > 6) {
+        //         console.log("Number is greater than 6")
+        //     }
+        // }
+
+        // let checkCharacters = function() {
+        //     let userName = prompt ('Enter your name');
+        //     console.log('Your name is' + userName.length + 'long')
+ 
+        // }
+        // checkCharacters ()
+        // function checkNumber() {
+        //     let num = prompt('Enter a number');
+        //     if (num > 6) {
+                
+        //     }
+        // }
+
+    //    let checkParameters = function 
+    //     let maxnum = prompt('Enter four numbers');
+    //     console.log('Highesst number')
+       
+
+    //    checkParameters ()
+
+        
+        
+
+
+// Another Method of Writing a  function
+// ARROW FUNCTION
+
+const greetMe = () => {
+    
+}
